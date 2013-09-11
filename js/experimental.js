47b08d826ebae56e85c424001ce223f80735765a
Oliver Randell <oliverrandell@gmail.com>
2013-09-11 11:07:48 +1000

    Changes to the folder structure to get rid of the crap to get the app to work on iOS

--- js/experimental.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$("#tab-bar a").click(function(e){
-		    e.preventDefault();
-		    var nextPage = $(e.target.hash);
-		    transition(nextPage, 'fade');
-		    $("#tab-bar").attr("className", e.target.hash.slice(1));
-		});
-	});
-});
